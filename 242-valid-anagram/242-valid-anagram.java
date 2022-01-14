764388fe7e28ba96882288ca77e207c9ef5c69a8
hellchang_dev <41244406+underdarks@users.noreply.github.com>
2022-01-14 13:11:33 +0900

    Time: 2 ms (96.10%), Space: 39 MB (90.53%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.java
+++ 242-valid-anagram/242-valid-anagram.java
@@ -4,11 +4,14 @@ class Solution {
             return false;
         char[] c1=s.toCharArray();
         char[] c2=t.toCharArray();
-        
         //오름차순 정렬
         Arrays.sort(c1);
         Arrays.sort(c2);
 
-        return Arrays.equals(c1,c2);
+        for (int i = 0; i < c1.length; i++) {
+            if(c1[i] != c2[i]) return false;
+        }
+        
+        return true;
     }
 }
