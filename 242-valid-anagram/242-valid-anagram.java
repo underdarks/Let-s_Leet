cdf9de8375130c8c72d2516629c6dcf65ece75d1
hellchang_dev <41244406+underdarks@users.noreply.github.com>
2022-01-14 13:09:22 +0900

    Time: 16 ms (17.94%), Space: 42.1 MB (12.89%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.java
+++ 242-valid-anagram/242-valid-anagram.java
@@ -1,17 +1,14 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
+      if (s.length() != t.length())
+            return false;
         char[] c1=s.toCharArray();
         char[] c2=t.toCharArray();
-        if(c1.length != c2.length)
-            return false;
         
         //오름차순 정렬
         Arrays.sort(c1);
         Arrays.sort(c2);
 
-        for (int i = 0; i < c1.length; i++) 
-            if(c1[i] != c2[i]) return false;
-
-        return true;
+        return Arrays.equals(c1,c2);
     }
 }
