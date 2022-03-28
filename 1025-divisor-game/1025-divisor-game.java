3cb1d5255423b9a294454a4822af80bb93ea432c
hellchang_dev <rkdrl45617@gmail.com>
2022-03-28 14:02:14 +0900

    Time: 0 ms (100.00%), Space: 38.8 MB (91.23%) - LeetHub

--- 1025-divisor-game/1025-divisor-game.java
+++ 1025-divisor-game/1025-divisor-game.java
@@ -1,8 +1,5 @@
 class Solution {
     public boolean divisorGame(int n) {
-        if(n%2 == 0)
-            return true;
-        
-        return false;
+        return n%2 == 0 ? true:false;
     }
 }
